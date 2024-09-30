79e43f0f9ca8bcfa82c4301b7a0476e2ffd09ade
Leguminous <murayaleo911@gmail.com>
2024-09-30 14:33:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(record){    
+      const wins = record.find(item => item.result === "W")
+      return wins ? wins.year : undefined
+}
+
+
+
+  
+
+
